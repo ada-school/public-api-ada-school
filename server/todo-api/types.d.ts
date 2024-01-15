4ea6e7ea80f6c4692bdde082d7533b6ec9f88385
ErikCasas <santicasas667@gmail.com>
2024-01-15 15:24:14 -0500

    Inteface for customRequest

--- server/todo-api/types.d.ts
+++ server/todo-api/types.d.ts
@@ -1,4 +1,11 @@
+import { Request } from 'express';
+import { ToDoModel } from './model/todo-model';
+
 export interface CustomError extends Error {
-    status?: number;
-    errors?: { message: string }[];
-  }
+  status?: number;
+  errors?: { message: string }[];
+}
+
+export interface CustomRequest extends Request {
+  todo?: ToDoModel;
+}
