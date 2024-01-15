88fb307d1e18b7383c094bcddd512a4655dbf5a5
ErikCasas <santicasas667@gmail.com>
2024-01-15 16:21:38 -0500

    spell

--- server/todo-api/controllers/controller.ts
+++ server/todo-api/controllers/controller.ts
@@ -1,13 +1,16 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import l from '../../common/logger';
+import { CustomRequest } from '../types';
+import { ToDoDBModel } from '../schemas/to-do-schema';
 // import { ToDoModel } from '../model/todo-model';
 // import errorHandler from '../middlewares/error.handler';
 
 export class Controller {
-  create(req: Request, res: Response): void {
+  async create(req: CustomRequest, res: Response): Promise<void> {
     try {
-      const data = req.body ;
-      res.send(data);
+      const newToDo = new ToDoDBModel(req.todo);
+      await newToDo.save();
+      res.json(newToDo);
     } catch (error) {
       l.info(error);
     }
