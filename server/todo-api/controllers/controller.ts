e1cbee91b4659f3ef1fc0abec7f40571b8c86d5a
ErikCasas <santicasas667@gmail.com>
2024-01-24 15:48:17 -0500

    type guards and new test

--- server/todo-api/controllers/controller.ts
+++ server/todo-api/controllers/controller.ts
@@ -1,37 +1,71 @@
 import { Response, NextFunction, Request } from 'express';
 import { ToDoDBModel } from '../schemas/to-do-schema';
-import { ToDoModel } from '../model/todo-model';
 import { Types } from 'mongoose';
 import { HTTPError } from '../types';
 import validator from 'validator';
 
 export class Controller {
   async create(req: Request, res: Response, next: NextFunction): Promise<void> {
-    const errors: string[] = [];
+    
+    const isObjectId = (
+      createdBy: string | Types.ObjectId
+    ): createdBy is Types.ObjectId => {
+      return Types.ObjectId.isValid(createdBy);
+    };
+
+    const isBoolean = (isCompleted: unknown): isCompleted is boolean => {
+      return typeof isCompleted === 'boolean';
+    };
+
+    const isString = (value: unknown): value is string => {
+      return typeof value === 'string';
+    };
+
+    const isNumber = (value: unknown): value is number => {
+      return typeof value === 'number';
+    };
+
+    const isDate = (value: unknown): value is Date => {
+      return value instanceof Date && !isNaN(value.getTime());
+    };
+
+    const haveMinRequiredValues = (
+      valuesInBodyRequest: Array<string>
+    ): boolean => {
+      return (
+        valuesInBodyRequest.includes('title') &&
+        valuesInBodyRequest.includes('isCompleted') &&
+        valuesInBodyRequest.includes('createdBy')
+      );
+    };
+
+    const errors: Array<string> = [];
 
     const { createdBy, isCompleted, description, priority, title, dueDate } =
-      req.body as ToDoModel;
-
-    if (createdBy && !Types.ObjectId.isValid(createdBy.toString())) {
+      req.body;
+    if (createdBy && !isObjectId(createdBy)) {
       errors.push('created by must by a objectId value');
     }
-    if (isCompleted && !validator.isBoolean(isCompleted.toString())) {
+    if (isCompleted && !isBoolean(isCompleted)) {
       errors.push('isCompleted by must by a boolean value');
     }
 
-    if (!validator.isLength(title, { min: 3, max: 15 })) {
+    if (isString(title) && !validator.isLength(title, { min: 3, max: 15 })) {
       errors.push(
         'title by must by a string value and with a length greater than 3 or less than or equal to 15'
       );
     }
-    if (description && !validator.isLength(description, { min: 3, max: 250 })) {
+    if (
+      isString(description) &&
+      !validator.isLength(description, { min: 3, max: 250 })
+    ) {
       errors.push(
         'description by must by a string value and with a length greater than 3 or less than or equal to 250'
       );
     }
 
     if (
-      priority &&
+      isNumber(priority) &&
       !validator.isInt(priority.toString(), { min: 0, max: 10 })
     ) {
       errors.push(
@@ -39,19 +73,27 @@ export class Controller {
       );
     }
 
-    if (dueDate && !validator.isISO8601(dueDate.toString())) {
+    if (
+      dueDate &&
+      !isDate(dueDate) &&
+      !validator.isISO8601(dueDate.toString())
+    ) {
       errors.push('dueDate must be a Date value');
     }
+    const valuesInBodyRequest = Object.keys(req.body);
 
+    let bodyErrors: HTTPError = {
+      status: 400,
+      name: 'Bad request',
+      message: 'The values provided through the request body are not correct',
+    };
     if (errors.length) {
-      const bodyErrors: HTTPError = {
-        status: 400,
-        name: 'Bad request',
-        message: 'The values provided through the request body are not correct',
+      bodyErrors = {
+        ...bodyErrors,
         errors: errors.map((message) => ({ message })),
       };
       next(bodyErrors);
-    } else {
+    } else if (haveMinRequiredValues(valuesInBodyRequest)) {
       try {
         const newToDo = new ToDoDBModel({
           createdBy,
@@ -66,6 +108,17 @@ export class Controller {
       } catch (error) {
         next(error);
       }
+    } else {
+      bodyErrors = {
+        ...bodyErrors,
+        errors: [
+          {
+            message:
+              'None of the properties provided in the body of the application are valid',
+          },
+        ],
+      };
+      next(bodyErrors);
     }
   }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -3,8 +3,8 @@ import Server from '../server/common/server';
 import routes from '../server/routes';
 import {
   cleanData,
-connectMemoryDB,
-closeMemoryDBConection
+  connectMemoryDB,
+  closeMemoryDBConection,
 } from './helper/mongoDbMemoryTestHelper';
 
 const server = new Server().router(routes);
@@ -16,14 +16,36 @@ const dataTest = {
   description: 'test description',
   priority: 1,
   title: 'tests',
-  dueDate: "2024-01-18T16:17:19.037Z",
+  dueDate: '2024-01-18T16:17:19.037Z',
 };
-const wrongDataTest = {
-  createdBy: '65a56547b8772ebf5f3c48',
+const wrongValuesDataTest = {
+  createdBy: '65a5d5d212a10d2a9879bc7',
   description: '--',
   priority: 12,
   title: '--',
 };
+
+const wrongKeysDataTest = {
+  test: 'test',
+  age: 21,
+  isFinished: true,
+};
+
+const arrayErrors = [
+  { message: 'created by must by a objectId value' },
+  {
+    message:
+      'title by must by a string value and with a length greater than 3 or less than or equal to 15',
+  },
+  {
+    message:
+      'description by must by a string value and with a length greater than 3 or less than or equal to 250',
+  },
+  {
+    message:
+      'priority by must by a number value less than 0 or equal to 10 or greater than or equal to 0',
+  },
+];
 
 describe('todo api server works', () => {
   beforeAll(async () => {
@@ -45,12 +67,23 @@ describe('todo api server works', () => {
     expect(response.statusCode).toBe(201);
   });
 
-  it('POST method only accepts correct information', async () => {
+  it('POST method only accepts correct values', async () => {
     const response = await request(app)
       .post('/api/v1/todos')
-      .send(wrongDataTest);
+      .send(wrongValuesDataTest);
     expect(response.statusCode).toBe(400);
     const { errors } = JSON.parse(response.text);
     expect(errors).toHaveLength(4);
+    expect(errors).toEqual(arrayErrors);
+  });
+  it('POST method only accepts correct information', async () => {
+    const response = await request(app)
+      .post('/api/v1/todos')
+      .send(wrongKeysDataTest);
+    expect(response.statusCode).toBe(400);
+    const { errors } = JSON.parse(response.text);
+    expect(errors[0].message).toBe(
+      'None of the properties provided in the body of the application are valid'
+    );
   });
 });
